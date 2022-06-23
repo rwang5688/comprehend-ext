584739d7709844d8bf068369036112b4c5c6bc3c
Robert Wang <rwang5688@outlook.com>
2022-06-22 23:54:10 -0700

    updatea invoke URL

--- src/background.js
+++ src/background.js
@@ -19,7 +19,7 @@ chrome.contextMenus.onClicked.addListener( ( info, tab ) => {
 } );
 
 const notify = message => {
-	var urlBase = 'https://z5a6imy452.execute-api.us-west-2.amazonaws.com/Prod/comprehend-api?';
+	var urlBase = 'https://e51stz2448.execute-api.us-west-2.amazonaws.com/Prod/comprehend-api?';
 	fetch(urlBase + 'message="' + message + '"')
 		.then(r => r.text())
 		.then(response => {
